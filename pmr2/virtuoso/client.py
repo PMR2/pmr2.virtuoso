dfe4954001b7d1cb32163157cda78c2f716a21a5
Tommy Yu <tommy.yu@auckland.ac.nz>
2014-06-20 14:56:36 +1200

    SparqlClient, web service version.
    - Provided anonymous access (leverage native read-only security).

---
+++ pmr2/virtuoso/client.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+from pmr2.virtuoso.sparql import quote_iri
+
+
+class SparqlClient(object):
+    """
+    A client for accessing the Virtuoso Sparql webservice endpoint.
+    """
+
+    def __init__(self, endpoint='http://localhost:8890/sparql',
+            requests_session=None):
+
+        if requests_session is None:
+            requests_session = requests.Session()
+
+        self.endpoint = endpoint
+        self.requests_session = requests_session
+
+    def query(self, sparql_query):
+        r = self.requests_session.get(self.endpoint, params={
+            'query': sparql_query,
+            'format': 'application/json',
+        })
+        return r.json()
