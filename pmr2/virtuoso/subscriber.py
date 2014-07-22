bb5f1d4cb67d597e1a20746cf63fd72010c2f41c
Tommy Yu <tommy.yu@auckland.ac.nz>
2014-07-22 15:42:58 +1200

    Limit the scope of the event
    - Now only triggered on incoming data to the underlying storage.

--- pmr2/virtuoso/subscriber.py
+++ pmr2/virtuoso/subscriber.py
@@ -3,12 +3,12 @@ import logging
 import zope.component
 
 from pmr2.app.workspace.interfaces import IWorkspace
-from pmr2.app.workspace.interfaces import IWorkspaceModifiedEvent
+from pmr2.app.workspace.interfaces import IWorkspaceStorageIncomingEvent
 from pmr2.virtuoso.interfaces import IWorkspaceRDFIndexer
 
 logger = logging.getLogger(__name__)
 
-@zope.component.adapter(IWorkspace, IWorkspaceModifiedEvent)
+@zope.component.adapter(IWorkspace, IWorkspaceStorageIncomingEvent)
 def reindex_workspace_rdf(workspace, event):
     try:
         zope.component.getAdapter(workspace, IWorkspaceRDFIndexer)()
