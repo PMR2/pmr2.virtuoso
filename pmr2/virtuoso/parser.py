7aa8543744f7758a92a375aedcc9230126832b08
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-06-23 17:00:36 +1200

    Cleaning up the parser module
    - It's good enough, deal with it.

--- pmr2/virtuoso/parser.py
+++ pmr2/virtuoso/parser.py
@@ -6,8 +6,8 @@ from rdflib import URIRef
 from rdflib.graph import Graph
 
 
-upstreamneedtolearntoknowwtfisrdf = \
-    'rdflibfailsathandlingrelativeurisorurnsoranything://oh/'
+upstreamneedtolearntoknowwtfisiri = \
+    'rdflibfailsathandlingrelativeirisorurnsoranything://oh/'
 
 
 def parse_rdfxml(rawstr):
@@ -23,40 +23,23 @@ def parse_rdfxml(rawstr):
     return result
 
 def parse_n3(rawstr):
-    def uri(self, tok):
-        return URIRef(tok)
     result = Graph()
-    # monkey patching
-    #from rdflib.plugins.parsers import notation3
-    #notation3.runNamespaceValue = ''
-    #base = lambda: ''
-    #notation3.base, original_base = base, notation3.base
-    ## parse with our much simpler URIRef generation that doesn't mangle
-    ## in the absolute file:// local path.
-
-    #result.parse(StringIO(rawstr), format='n3')
-    #result.parse(StringIO(rawstr), format='n3', publicID='#')
-
     result.parse(StringIO(rawstr), format='n3',
-        publicID=upstreamneedtolearntoknowwtfisrdf)
+        publicID=upstreamneedtolearntoknowwtfisiri)
 
-    def nukeit(node):
+    def torelative(node):
         if not isinstance(node, URIRef):
             return node
-        if node.startswith(upstreamneedtolearntoknowwtfisrdf):
-            return URIRef(node.replace(upstreamneedtolearntoknowwtfisrdf, ''))
+        if node.startswith(upstreamneedtolearntoknowwtfisiri):
+            return URIRef(node.replace(upstreamneedtolearntoknowwtfisiri, ''))
         return node
             
     real_result = Graph()
     for triple in result:
         s, p, o = triple
-        if 'simple.n3' in s:
-            import pdb;pdb.set_trace()
-        real_result.add((nukeit(s), nukeit(p), nukeit(o)))
+        real_result.add((torelative(s), torelative(p), torelative(o)))
     return real_result
               
-    ## undo our monkey patching.
-    #notation3.base = original_base
     return result
 
 def parse(rawstr):
