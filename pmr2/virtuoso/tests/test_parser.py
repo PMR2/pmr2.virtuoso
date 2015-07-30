b5d5223865f596186d5d85a3133afdc8717cfce8
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-07-30 17:08:40 +1200

    A simple turtle test case
    - Seems like rdflib can parse turtle with the n3 parser?

--- pmr2/virtuoso/tests/test_parser.py
+++ pmr2/virtuoso/tests/test_parser.py
@@ -9,7 +9,7 @@ class ParserTestCase(unittest.TestCase):
 
     def setUp(self):
         self.r1 = {}
-        for fn in ['simple.n3', 'simple.rdf', 'embedded.rdf']:
+        for fn in ['simple.n3', 'simple.rdf', 'embedded.rdf', 'turtle.ttl']:
             with open(join(dirname(testing.__file__), 'data', '0', fn)) as fd:
                 self.r1[fn] = fd.read()
 
@@ -20,6 +20,10 @@ class ParserTestCase(unittest.TestCase):
     def test_simple_n3_parse(self):
         graph = parser.parse(self.r1['simple.n3'])
         self.assertEqual(len(list(graph)), 2)
+
+    def test_simple_turtle_parse(self):
+        graph = parser.parse(self.r1['turtle.ttl'])
+        self.assertEqual(len(list(graph)), 14)
 
     def test_embedded_rdf_parse(self):
         graph = parser.parse(self.r1['embedded.rdf'])
